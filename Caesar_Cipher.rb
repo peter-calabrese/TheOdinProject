a581998be7cf4e50572bd992154be75fb64159f7
Peter Calabrese <peter@calabrese.tech>
2022-12-09 23:03:53 -0500

    added Caesar Cipher Solution

---
+++ Caesar_Cipher.rb
@@ -0,0 +1,23 @@
+$alphabet = Array.new
+$encrypted_text = Array.new
+("a".."z").map {|letter| $alphabet.push(letter)}
+
+def CaesarsCipher(word, shift)
+  word.each_char {|letter|
+    if  letter.match(/[A-Z]/)
+      new_index =  $alphabet.find_index(letter.downcase) + shift
+      new_index > 26 ? new_index -= 26 : new_index
+      $encrypted_text.push($alphabet[new_index].upcase)
+    elsif letter.match(/[a-z]/)
+      new_index =  $alphabet.find_index(letter) + shift
+      new_index > 26 ? new_index -= 26 : new_index
+      $encrypted_text.push($alphabet[new_index])
+    else
+      $encrypted_text.push(letter)
+    end
+  }
+  puts $encrypted_text.join("")
+  $encrypted_text.clear
+end
+
+CaesarsCipher("What a string!", 5)
