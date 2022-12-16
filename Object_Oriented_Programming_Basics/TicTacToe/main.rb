068f2deeaa48fdfe09100e68915560d864515eef
Peter Calabrese <peter@calabrese.tech>
2022-12-15 21:34:56 -0500

    Added play logic (needs to be worked on)

--- Object_Oriented_Programming_Basics/TicTacToe/main.rb
+++ Object_Oriented_Programming_Basics/TicTacToe/main.rb
@@ -3,20 +3,33 @@ require_relative 'lib/player.rb'
 # -------------Instantiate Players and Game--------------------
 puts "Player 1 Name: "
 first_player_name = gets.chomp
-puts "Player 1 Shape: "
-first_player_shape = gets.chomp
+first_player_shape = "x"
 puts "Player 2 Name: "
 second_player_name = gets.chomp
-puts "Player 2 Shape: "
-second_player_shape = gets.chomp
+second_player_shape = "o"
 first_player = Player.new(first_player_name, first_player_shape)
 second_player = Player.new(second_player_name, second_player_shape)
 game = Game.new(first_player, second_player)
 #--------------Start Game----------------------
 game.intro
 playing = true
+player_turn = "player1"
 while playing
-  puts "Where would you like to go?"
-  input = gets
+  if player_turn === "player1"
+    input = game.get_player_input
+    valid_move = game.is_space_free(input)
+    if valid_move ? continue : break
+      player_turn = "player2"
+    else
+      input = Game.get_player_input
+      player_turn = "player1"
+    end
+    if input.between?(1, 9)
+      ans = "test"
+      puts ans
+    else
+      puts "tits"
+    end
+  end
 end
 
