aad01e7e9d8facb74569cc7dee58741490db6c2b
Peter Calabrese <peter@calabrese.tech>
2022-12-15 20:42:19 -0500

    include player class in main

--- Object_Oriented_Programming_Basics/TicTacToe/main.rb
+++ Object_Oriented_Programming_Basics/TicTacToe/main.rb
@@ -1,9 +1,22 @@
-# frozen_string_literal: true
-
 require_relative 'lib/game.rb'
-
-game = Game.new("Peter", "Gina")
-
+require_relative 'lib/player.rb'
+# -------------Instantiate Players and Game--------------------
+puts "Player 1 Name: "
+first_player_name = gets.chomp
+puts "Player 1 Shape: "
+first_player_shape = gets.chomp
+puts "Player 2 Name: "
+second_player_name = gets.chomp
+puts "Player 2 Shape: "
+second_player_shape = gets.chomp
+first_player = Player.new(first_player_name, first_player_shape)
+second_player = Player.new(second_player_name, second_player_shape)
+game = Game.new(first_player, second_player)
+#--------------Start Game----------------------
 game.intro
-
+playing = true
+while playing
+  puts "Where would you like to go?"
+  input = gets
+end
 
